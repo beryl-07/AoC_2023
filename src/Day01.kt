7292063efc2241e200387085e88ba56a9533e56f
Béryl HOUESSOU <berylhoe@gmail.com>
2023-12-01 10:14:37 +0100

    Day 1 puzzle 1 refactor

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,22 +1,31 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
     fun part2(input: List<String>): Int {
         return input.size
     }
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+    /*
+     Puzzle 01
+      var count = 0
+      input.forEach { el ->
+          var str = ""
+          el.forEach {
+              if(it.isDigit())
+                  str += it
+          }
+          count += (str[0]+str[(str.length-1)].toString()).toInt()
+      }
+      print(count)
+    */
 
     val input = readInput("Day01_test/input0")
 
+   /* Puzzle 01 . Refactor */
     var count = 0
     input.forEach { el ->
-        var str = ""
-        el.forEach {
-            if(it.isDigit())
-                str += it
-        }
-        count += (str[0]+str[(str.length-1)].toString()).toInt()
+        val str = el.filter { it.isDigit() }
+        count += (str[0] + str.last().toString()).toInt()
     }
     print(count)
 }
